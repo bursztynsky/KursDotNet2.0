9fc685401c1f29a7e23b61cf45847ac2a6fb5783
Tomasz Bursztyński <bursztynski.tomasz@gmail.com>
2023-03-03 09:59:45 +0100

    Add project 10-PlikiFolderyStrumienie

---
+++ 10-PlikiFolderyStrumienie/FilesService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_PlikiFolderyStrumienie
+{
+    public class FilesService
+    {
+        // readonly -> powoduje, ze zmienna moze miec ustawiona wartosc TYLKO w konstruktorze LUB przy inicie tzn. ze wygladaloby tak: private readonly string _pathToDirectory = "test";
+        private readonly string _pathToDirectory;
+
+        public FilesService(string pathToDirectory)
+        {
+            _pathToDirectory = pathToDirectory;
+        }
+
+        public void ShowDirectories()
+        {
+            var directories = Directory.GetDirectories(_pathToDirectory);
+
+            foreach (var dir in directories)
+            {
+                Console.WriteLine(dir);
+            }
+        }
+
+        public void ShowFiles()
+        {
+            var files = Directory.GetFiles(_pathToDirectory);
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        public void ShowAll()
+        {
+            ShowDirectories();
+            ShowFiles();
+        }
+
+        public void DeleteAll()
+        {
+            Directory.Delete(_pathToDirectory, true);
+        }
+
+        public void GenerateRandomValues()
+        {
+            var min = 10;
+            var max = 15;
+
+            var random = new Random();
+
+            var numberOfDirectories = random.Next(min, max);
+
+            Console.WriteLine("Generating " + numberOfDirectories + " directories...");
+
+            var folderName = "generatedFolder";
+            var fileName = "generatedFile";
+            var fileExtension = ".txt";
+
+            for (int i = 1; i <= numberOfDirectories; i++)
+            {
+                // tworze folder o nazwie np. generatedFolder1, generatedFolder2 itd.
+                // $ przed string nazywa sie interpolacja z ang. string interpolation
+                var newDirectory = $"{_pathToDirectory}\\{folderName}{i}";
+
+                if (!Directory.Exists(newDirectory))
+                {
+                    Directory.CreateDirectory(newDirectory);
+                }
+
+                // jesli wylosuje liczbe podzielna przez 2 to beda w srodku generowane pliki
+                var shouldBeEmpty = random.Next(0, 50) % 2 == 0;
+
+                if (shouldBeEmpty)
+                {
+                    var numberOfFiles = random.Next(min, max);
+
+                    Console.WriteLine("Generating " + numberOfFiles + " files...");
+
+                    for (int j = 1; j <= numberOfFiles; j++)
+                    {
+                        //File.Create("newDirectory" + "\\" + fileName + j + fileExtension); // to jest rowniez ok wiec jak wolicie
+                        var newFileName = $"{newDirectory}\\{fileName}{j}{fileExtension}";
+
+                        if (!File.Exists(newFileName))
+                        {
+                            File.Create(newFileName);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 10-PlikiFolderyStrumienie/Program.cs
@@ -0,0 +1,98 @@
+﻿//--------------------
+// OPIS:
+// - aplikacja prosi uzytkownika o podanie sciezki do folderu
+// - aplikacja umozliwia usuwanie wszystkie w danej sciezce
+// - aplikacja umozliwia generowanie testowych plikow i folderow w danej sciezce
+// - aplikacja umozliwia wyswietlenie wszystkich plikow i folderow i podfolderow i wszystkiego co jest w danej sciezce
+// - aplikacja umozliwia zmiane prefix i sufix dla wszystkich plikow w podanej sciezce
+//--------------------
+
+using _10_PlikiFolderyStrumienie;
+
+var shutDown = false;
+var directoryPath = "";
+
+Console.WriteLine("Hello in the 'directories and folders' program!");
+
+while (!shutDown)
+{
+    // Umozliwiamy uzytownikowi podanie sciezki do folderu w ktorym chcemy wykonywac operacje
+    // Co musimy zrobic:
+    // - odebrac sciezke od usera
+    // - sprawdzic czy nie jest pusta
+    // - sprawdzic czy istnieje a jesli nie, to ja utworzyc
+
+    Console.WriteLine("Please provide the path to directory:");
+
+    //directoryPath = Console.ReadLine();
+    // W ramach testowania ustawie na sztywno ze path to: C:\Users\tom\Desktop\TEST_PROGRAMU
+    directoryPath = "C:\\Users\\tom\\Desktop\\TEST_PROGRAMU";
+
+    // ustawiam shutDown na true by wyjsc z tego while bo mam juz sciezke do folderu
+    shutDown = true;
+}
+
+var service = new FilesService(directoryPath);
+
+// ustawiam shutDown na true by wejsc do nastepnego while
+shutDown = false;
+
+while (!shutDown)
+{
+    if (string.IsNullOrWhiteSpace(directoryPath))
+    {
+        Console.WriteLine("You have provided an empty value!");
+        continue;
+    }
+
+    if (!Directory.Exists(directoryPath))
+    {
+        // jesli nie ma tkaiej sciezki, to tworzymy
+        Directory.CreateDirectory(directoryPath);
+    }
+
+
+    Console.WriteLine("\nList of possible operations:");
+    Console.WriteLine("Type [1] Show all files and directories in the path");
+    Console.WriteLine("Type [2] Remove all files and directories in the path");
+    Console.WriteLine("Type [3] Generates random files and directories in the path");
+    Console.WriteLine("Type [EXIT] for closing the app");
+
+    var operation = Console.ReadLine();
+
+    // ReadLine moze zwrocic null, (jak wpiszemy ^Z czyli Ctrl+Z) wiec musimy sprawdzic czy nim nie jest by nie dostac bledu
+    //if (operation == null)
+    //{
+    //    Console.WriteLine("Don't user Ctrl+Z!");
+    //    continue;
+    //}
+
+    // Drugim rozwiazaniem jest dodanie do zmiennej (przed .) znaku ?
+    // Korzystanie z '?.' powoduje, ze program wie, ze to co jest przed . MOZE byc nullem, wiec sam sobie w srodku
+    // korzysta z takiego 'try-catch' czyli nie walnie nam bledem!
+    // TZN. ze jesli operation jest null i ja odpale linie 'operation.ToUpper()' to bede mial NullReferenceException
+    // a jesli uzyje 'operation?.ToUpper()' to bledu nie bedzie i zwroci nam znowu wartosc null
+
+    switch (operation?.ToUpper())
+    {
+        case "1":
+            service.ShowAll();
+            break;
+        case "2":
+            service.DeleteAll();
+            break;
+        case "3":
+            service.GenerateRandomValues();
+            break;
+        case "EXIT":
+            shutDown = true;
+            break;
+        default:
+            Console.WriteLine("You've provided the wrong operation!");
+            break;
+    }
+}
+
+//carService.SaveData();
+
+Console.WriteLine("Closing the application...");
