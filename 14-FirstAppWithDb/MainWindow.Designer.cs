465f3f240c4587edce151b53be0891746dda3f2d
Tomasz Bursztyński <bursztynski.tomasz@gmail.com>
2023-04-17 20:22:29 +0200

    First step with adding db application

---
+++ 14-FirstAppWithDb/MainWindow.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace _14_FirstAppWithDb
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            selectFileButton = new Button();
+            dataView = new DataGridView();
+            CarName = new DataGridViewTextBoxColumn();
+            CarModel = new DataGridViewTextBoxColumn();
+            CarYear = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataView).BeginInit();
+            SuspendLayout();
+            // 
+            // selectFileButton
+            // 
+            selectFileButton.Location = new Point(350, 45);
+            selectFileButton.Name = "selectFileButton";
+            selectFileButton.Size = new Size(100, 50);
+            selectFileButton.TabIndex = 0;
+            selectFileButton.Text = "Select file to load data";
+            selectFileButton.UseVisualStyleBackColor = true;
+            selectFileButton.Click += selectFileButton_Click;
+            // 
+            // dataView
+            // 
+            dataView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataView.Columns.AddRange(new DataGridViewColumn[] { CarName, CarModel, CarYear });
+            dataView.Location = new Point(130, 139);
+            dataView.Name = "dataView";
+            dataView.RowTemplate.Height = 25;
+            dataView.Size = new Size(553, 254);
+            dataView.TabIndex = 1;
+            // 
+            // CarName
+            // 
+            CarName.HeaderText = "CarName";
+            CarName.Name = "CarName";
+            // 
+            // CarModel
+            // 
+            CarModel.HeaderText = "CarModel";
+            CarModel.Name = "CarModel";
+            // 
+            // CarYear
+            // 
+            CarYear.HeaderText = "CarYear";
+            CarYear.Name = "CarYear";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(566, 59);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(dataView);
+            Controls.Add(selectFileButton);
+            Name = "MainWindow";
+            Text = "Moje glowne okienko";
+            FormClosed += MainWindow_FormClosed;
+            ((System.ComponentModel.ISupportInitialize)dataView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button selectFileButton;
+        private DataGridView dataView;
+        private DataGridViewTextBoxColumn CarName;
+        private DataGridViewTextBoxColumn CarModel;
+        private DataGridViewTextBoxColumn CarYear;
+        private Button button1;
+    }
+}
---
+++ 14-FirstAppWithDb/MainWindow.cs
@@ -0,0 +1,79 @@
+using _14_FirstAppWithDb.Models;
+using System.Text;
+
+namespace _14_FirstAppWithDb
+{
+    public partial class MainWindow : Form
+    {
+        private readonly DataService _dataService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _dataService = new DataService();
+
+            InitDataView();
+        }
+
+        public void LoadData(string filePath)
+        {
+            _dataService.GetCarsFromFile(filePath);
+
+            ResetDataView();
+        }
+
+        private void selectFileButton_Click(object sender, EventArgs e)
+        {
+            var filePathWindow = new FilePathWindow(this);
+
+            //filePathWindows.Show(); // otwiera okno I NIE BLOKUE OKNA-MATKI wiec mozesz utworzyc miliard tych okienek
+            filePathWindow.ShowDialog(); // blokuje okno matka wiec nie moge klikac juz w poprzednie okno
+            // poki nie zamkne tego
+
+            var filePath = filePathWindow.FilePath;
+
+            if (string.IsNullOrWhiteSpace(filePath))
+            {
+                return;
+            }
+        }
+
+        private void InitDataView()
+        {
+            _dataService.InitData();
+
+            ResetDataView();
+        }
+
+        private void ResetDataView()
+        {
+            var dataView = Controls
+                .Find("dataView", true)
+                .FirstOrDefault()
+                as DataGridView;
+
+            dataView.Rows.Clear();
+
+            foreach (var car in _dataService.Cars)
+            {
+                var newRowId = dataView.Rows.Add();
+                var newRow = dataView.Rows[newRowId];
+
+                newRow.Cells["CarName"].Value = car.Name;
+                newRow.Cells["CarModel"].Value = car.Model;
+                newRow.Cells["CarYear"].Value = car.Year;
+            }
+        }
+
+        private void MainWindow_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _dataService.SaveCarsToFile();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _dataService.GetAllFromDb();
+        }
+    }
+}
